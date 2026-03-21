36902c7c7a5de23fcb0f045d51b22d82af4282d5
audir8-5 <audir8.5332@gmail.com>
2026-03-21 19:47:45 +0530

    50% code

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
+let lock=false
 async function prt(e){
   e.preventDefault();
-  console.log("RUN clicked");
-
-  try{
-    let x = document.getElementById("container").value;
-
-    const res = await fetch("http://localhost:8000/run", {
-      method: "POST",
-      headers: { 'Content-Type': 'text/plain' },
-      body: x
-    });
-
-    console.log("Response received", res);
-
-    const text = await res.text();   // 👈 NOT json yet
-    console.log("RAW RESPONSE:", text);
-
-    const result = JSON.parse(text); // 👈 force parse
-
-    document.getElementById("output").value = result.output;
-
-  } catch(err){
-    console.log("ERROR:", err);
+  if(lock)return
+    
+    try{
+      lock=true;
+      showloading(true);
+      let x = document.getElementById("container").value;
+      const res = await fetch("http://localhost:8000/run", {
+        method: "POST",
+        headers: { 'Content-Type': 'text/plain' },
+        body: x
+      });
+      
+      console.log("Response received", res);
+      
+      const text = await res.text();   // 👈 NOT json yet
+      console.log("RAW RESPONSE:", text);
+      
+      const result = JSON.parse(text); // 👈 force parse
+      
+      document.getElementById("output").value = result.output;
+      
+    } catch(err){
+      console.log("ERROR:", err);
+    }finally{
+      lock=false
+      showloading(false)
+    }
+}
+function showloading(val){
+  let btn=document.getElementById("load")
+  if(val)
+  {
+    btn.textContent = "Compiling..."; // Added quotes
+    btn.style.opacity = "0.5";         // Visual logic: dim the button
+    btn.style.pointerEvents = "none";
+  }
+  else{
+    btn.textContent="Run";
+    btn.style.opacity = "1";
+    btn.style.pointerEvents = "auto";
   }
 }
-
 function reset() {
   document.getElementById("container").value = "";
   document.getElementById("output").value = "";
--- server/api/log.js
+++ server/api/log.js
@@ -1,10 +1,9 @@
-import { appendFile } from 'fs'
-async function logging(req,res,next){
+import { appendFile, appendFileSync } from 'fs'
+function logging(req,res,next){
     try
     {
-
         const data=`\n${new Date().toLocaleTimeString()}     ${req.url}`
-        await appendFile("./logs/.logs.txt",data,(err)=>{
+        appendFileSync("./logs/.logs.txt",data,(err)=>{
             if(err)console.log(err)
         })
         }catch(err){
